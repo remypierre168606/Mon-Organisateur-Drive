77980891d686c4461f3f3bbd426d2ac67bef974d
remypierre168606 <chrirousseau@gmail.com>
2026-07-01 05:36:46 +0200

    Add files via upload

--- app.js
+++ app.js
@@ -1386,7 +1386,7 @@ if($('companyInfoForm')){
 
 // ---------------- GOOGLE DRIVE SYNC ----------------
 
-const VERSION_LABEL = 'V51.1';
+const VERSION_LABEL = 'V52 Mobile';
 let driveConnectedForBanner = false;
 let lastSaveTimeForBanner = localStorage.getItem('mon-organiseur-last-save-time') || '--';
 let lastLocalSaveTimeForBanner = localStorage.getItem('mon-organiseur-last-local-save-time') || '--';
